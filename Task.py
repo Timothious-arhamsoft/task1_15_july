0f068bebf22efb357ca1815a6b446055fffdeb55
Timothious Gill <timothious.gill@arhamsoft.com>
2026-07-15 19:25:15 +0500

    Task 1

---
+++ Task.py
@@ -0,0 +1,39 @@
+
+
+
+#*args and **kwargs
+
+def func1(*args):
+    print(args)
+
+func1(1, 2, 3)
+
+def func2(**kwargs):
+    print(kwargs)
+
+
+func2(name="check", age=23)
+
+
+def student(name, age, city):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"City: {city}")
+
+
+# Positional arguments
+student("Ali", 22, "Lahore")
+
+print()
+
+# Keyword arguments
+student(name="Sara", age=20, city="Karachi")
+
+print()
+
+# Different order
+student(city="Islamabad", name="Ahmed", age=25)
+
+# check for testing
+def greet(name):
+    print(name) 
